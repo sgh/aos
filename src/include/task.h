f27cf479038015c13d1dae3f753ff00546c45598
Søren Holm <sgh@sgh.dk>
2006-07-09 20:49:15 +0000

    doxygen

--- src/include/task.h
+++ src/include/task.h
@@ -5,14 +5,41 @@
 #include <list.h>
 
 struct task_t {
-	char* name;								// The name of the task
-	struct list_head q;		// Queue
+	/**
+	 * \brief The name of the task.
+	 */
+	char* name;
+	
+	/**
+	 * \brief Node in process-queue.
+	 */
+	struct list_head q;	
+	
 	enum task_state {RUNNING, READY, BLOCKED} state;
-	uint32 sleep_time;				// useconds to sleep
+	
+	/**
+	 * \brief  useconds to sleep.
+	 */
+	uint32 sleep_time;
+	
 	funcPtr entrypoint;
-	REGISTER_TYPE *stack;			// Pointer to the stack
-	uint16 stack_size;				// Size of the space pointed to by *stack
-	uint8 stacksave[500];
+	
+	/**
+	 * \brief Pointer to the process' saved stack. if the process is using the systems shared stack.
+	 */
+	REGISTER_TYPE *stack;
+	
+	/**
+	 * \brief Size of the space pointed to by *stack
+	 * If the size is greater than zero, the process is using shared stacks.
+	 */
+	uint16 stack_size;
+	
+	/**
+	 * \brief  This will hold the process' complete stack if this process is uses a seperate stack. ÆØÅæøå
+	 * If running with shared stack this space only contains the saved processor-state.
+	 */
+	uint8 stacksave[500]; /**< @todo convert this to at pointer to the memory-space */
 };
 
 #define get_struct_task(Q) (container_of(Q,struct task_t,q))
