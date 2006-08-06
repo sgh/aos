4bab929b7976081e4dbebc6ca96c650dbc6021cf
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2006-08-06 22:07:52 +0000

    SUCCESS error-code
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@39 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/include/errno.h
+++ src/include/errno.h
@@ -1,6 +1,7 @@
 #ifndef ERRNO_H
 #define ERRNO_H
 
+#define ESUCCESS     0  /* No error */
 #define ENOIOCTL     1  /* No IO-Controls available */
 #define EINVALIOCTL  2  /* Invalid IO-Control */
 #define ENODEV       3  /* No such device */
