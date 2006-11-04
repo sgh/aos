113d4c4295594eba3ef9ed44c345e06d13ae9445
Søren Holm <sgh@sgh.dk>
2006-11-04 22:51:20 +0000

    semaphores are not not system-calls. Added mutex-lock/unlock in semaphore routines Added sem_init to initialize semaphores. Refs #15

--- src/include/semaphore.h
+++ src/include/semaphore.h
@@ -2,13 +2,16 @@
 #define SEMAPHORE_H
 
 #include <types.h>
+#include <mutex.h>
 #include <list.h>
 
 struct semaphore_t {
-	int16_t counter;
+	int16_t count;
+	mutex_t lock;
 	struct list_head waiting;
 };
 
+void sem_init(struct semaphore_t* s, uint16_t count);
 void sem_down(struct semaphore_t* s);
 void sem_up(struct semaphore_t* s);
 
