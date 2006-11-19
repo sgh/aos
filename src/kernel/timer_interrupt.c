aad83c8aad201a9baf95f41d819edfc04f186616
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2006-11-19 00:28:17 +0000

    Time-slace based on time to next process-wake-up. See #10 and #11.
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@89 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/kernel/timer_interrupt.c
+++ src/kernel/timer_interrupt.c
@@ -11,8 +11,9 @@
 static int8_t vector_num;
 
 void init_timer_interrupt() {
-	T0_PR = 15000000/1000;		/* Scale to 1 ms steps */
-	T0_MR0 = 1;								/* Match-Register0 */
+	T0_PR = 15000000/1000000 - 1;		/* Scale to 1 ms steps */
+// 	T0_PC = 0;
+	T0_MR0 = 1000;								/* Match-Register0 */
 	
 // 	VICVectCntl0 = 4 + BIT5;
 // 	VICVectAddr0 = (uint32)timer_interrupt;
@@ -21,7 +22,7 @@ void init_timer_interrupt() {
 	if ( vector_num == -1 )
 		for (;;);
 	
-	T0_MCR = BIT0 | BIT1;		/* Interrupt on Math-Register0 */
+	T0_MCR = BIT0 | BIT1;		/* Reset and Interrupt on Math-Register0 */
 	T0_TCR = BIT0;					/* Enable timer0 */
 }
 
@@ -45,13 +46,11 @@ void timer_interrupt_routine() {
 	struct task_t* t;
 	struct list_head* e;
 	uint32_t past_time;
-	static uint8_t count= 0;
+	static uint16_t count= 0;
 	static uint8_t onoff = 0;
-	 
-	T0_IR = BIT0; /* Clear interrupt */
-	VICVectAddr = 0; /* Update priority hardware */
+	uint32_t time_to_wake = 1000;
 	
-	if (count == 20) {
+	if (count == 50) { // Do 10 Hz blink
 		if (onoff)
 			GPIO1_IOSET = BIT24;
 		else
@@ -78,10 +77,27 @@ void timer_interrupt_routine() {
 			if (t->sleep_time == 0) { // If process now has no time left to sleep
 				list_erase(/*&msleepQ,*/&t->q);
 				list_push_front(&readyQ,&t->q);
-
 			}
+
+			if (!list_isempty(&msleepQ)) {
+				e = list_get_front(&msleepQ); // Set e to the next to wake up
+				t = get_struct_task(e); // Get task-struct.
+
+				time_to_wake = t->sleep_time;
+				if (time_to_wake < 10) /** TODO 10 should be the time of a timerinterrupt times 2 or larger */
+					time_to_wake = 10;
+
+				if (time_to_wake > 1000) // This insured at least 100 interrupts pr. sec.
+					time_to_wake = 1000;
+			}
+
 		}
+
+	T0_MR0 = time_to_wake;
 	
-	T1_TC = 0;	// Reset timer to zero
+	T1_TC = 0;	// Reset delay-timer to zero
 	do_context_switch = 1;
+
+	T0_IR = BIT0; /* Clear interrupt */
+	VICVectAddr = 0; /* Update priority hardware */
 }
