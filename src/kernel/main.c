6e477017f0554c4a4e8e344377fc799a3505094c
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2006-12-09 22:30:20 +0000

    moved aos again from main.c
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@100 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -11,25 +11,12 @@
 #include <serio.h>
 #include <mm.h>
 
-struct task_t* current = NULL;
-REGISTER_TYPE task1_stack[17];
-REGISTER_TYPE task2_stack[17];
-REGISTER_TYPE idle_stack[17];
-
-struct task_t task1_cd;
-struct task_t task2_cd;
-struct task_t idle_cd;
-
-
-// const uint32 entrypoint_offset = offsetof(struct task_t,entrypoint);
-const uint32_t context_offset = offsetof(struct task_t,context);
-
-uint32_t global_int = 0;
-
+struct task_t* task1_cd;
+struct task_t* task2_cd;
+struct task_t* idle_cd;
 
 mutex_t mymutex;
 
-int p;
 
 void /*__attribute__((noreturn)) __attribute__((nothrow))*/ task1(void) {
 // 	uint8 c;
@@ -123,36 +110,6 @@ void /*__attribute__((noreturn)) __attribute__((nothrow))*/ task2(void) {
 	}
 }
 
-static void init_task(struct task_t* task,funcPtr entrypoint,REGISTER_TYPE* stack) {
-// 	memset( (void*)stack, 0, 64);
-	memset(task, 0, sizeof(struct task_t));
-	task->context = stack;
-// 	task->fragment = NULL;
-	task->context[0] = (uint32_t)(entrypoint);                                  // Entrypoint
-#ifdef SHARED_STACK
-	task->context[1] = (uint32_t)&Top_Stack;  // Shared stack SP
-#else
-	task->context[1] = (uint32_t)task->context + (1024 * sizeof(REGISTER_TYPE));  // Seperate stack SP
-#endif
-	task->context[2] = 0x12345678; // LR
-	task->context[3] = 0; // r0
-	task->context[4] = 0x60000010;  // SPSR User-mode
-	task->context[5] = 0x1; // r1
-	task->context[6] = 0x2; // r2
-	task->context[7] = 0x3; // r3
-	task->context[8] = 0x4; // r4
-	task->context[9] = 0x5; // r5
-	task->context[10] = 0x6; // r6
-	task->context[11] = 0x7; // r7
-	task->context[12] = 0x8; // r8
-	task->context[13] = 0x9; // r9
-	task->context[14] = 0x10; // r10
-	task->context[15] = 0x11; // r11
-	task->context[16] = 0x12; // r12
-	task->state = READY;
-	list_push_back(&readyQ,&task->q);
-}
-
 
 void /*__attribute__((weak)) __attribute__((noreturn)) __attribute__((nothrow))*/ idle_task()  {
 	uint8_t onoff = 0;
@@ -173,48 +130,9 @@ void /*__attribute__((weak)) __attribute__((noreturn)) __attribute__((nothrow))*
 	}
 }
 
-typedef void (*funcptr)();
-
-// Linker provides theese
-extern funcptr __start_driver_initcalls[];
-extern funcptr __stop_driver_initcalls[];
-extern funcptr __start_bus_initcalls[];
-extern funcptr __stop_bus_initcalls[];
-extern funcptr __start_class_initcalls[];
-extern funcptr __stop_class_initcalls[];
-
-
-
-void do_initcalls() {
-	funcptr* initcall;
-	
-	// Init classes
-	initcall = __start_class_initcalls;
-	while (initcall != __stop_class_initcalls) {
-		(*initcall)();
-		initcall++;
-	}
-	
-	// Init busses
-	initcall = __start_bus_initcalls;
-	while (initcall != __stop_bus_initcalls) {
-		(*initcall)();
-		initcall++;
-	}
-	
-	// Init drivers
-	initcall = __start_driver_initcalls;
-	while (initcall != __stop_driver_initcalls) {
-		(*initcall)();
-		initcall++;
-	}
-
-}
 
 
 char __attribute__((aligned(4))) dmem[5*1024];
-
-void mm_init(void* start, unsigned short len);
 
 struct device lpcuart = {
 	.name = "lpcuart",
@@ -225,21 +143,21 @@ struct device lpcuart = {
 void main(void) {
 	GPIO1_IODIR |= BIT24|BIT23|BIT22;
 	
-	do_initcalls();
+	aos_basic_init();
 	
 	mm_init(dmem, sizeof(dmem));
 	
 	device_register(&lpcuart);
 	
-	init_task(&task1_cd,task1,task1_stack);
-	init_task(&task2_cd,task2,task2_stack);
-	init_task(&idle_cd,idle_task,idle_stack);
+	task1_cd = malloc(sizeof(struct task_t));
+	task2_cd = malloc(sizeof(struct task_t));
+	idle_cd = malloc(sizeof(struct task_t));
+
+	init_task(task1_cd,task1);
+	init_task(task2_cd,task2);
+	init_task(idle_cd,idle_task);
 	
 	mutex_init(&mymutex);
 	
-	init_timer_interrupt();
-	enable_timer_interrupt();
-		
-	current = NULL;
-	yield();
+	aos_context_init();
 }
