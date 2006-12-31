4c4ff68bbe54c8e951eb354174c0c84392a9834d
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2006-12-31 01:09:34 +0000

    cpu-usage. Not implemented
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@147 01777da7-cf65-4787-a2ab-fdced703dd4c

---
+++ src/kernel/cpuusage.c
@@ -0,0 +1,18 @@
+
+#include <types.h>
+
+/**
+ * \brief Timer-value when usage stats where last recorded
+ */
+static uint32_t last_usage_point;
+
+
+void cpuusage_system() {
+}
+
+void cpuusage_user() {
+	
+}
+
+void cpuusage_idle() {
+}
