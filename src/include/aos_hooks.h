5bcb4a4465c6052a25fc19d40a1af03ff6b5789e
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2007-01-03 05:05:22 +0000

    missing headers
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@157 01777da7-cf65-4787-a2ab-fdced703dd4c

---
+++ src/include/aos_hooks.h
@@ -0,0 +1,60 @@
+#ifndef AOS_HOOKS_H
+#define AOS_HOOKS_H
+
+/**
+ * \brief This struct contains hook-functions to call at different conditions.
+ *
+ * Theese conditions may not be fatal, but could be.
+ * Therefor the hooks should be carefull not to do things that
+ * rely on the failing functionality. The hooks are postfixed with 'error',
+ * 'fatal' or 'event' to indicate the severeness of the condition.
+ *
+ * All of these functions are called in privileged mode (system-mode) and
+ * one should therefore be carefull to do much more than vital stuff.
+ */
+struct aos_hooks {
+  /**
+	 * \brief Out-Of-Memory handler called when malloc can not allocate the
+	 * requested piece of memory.
+	 *
+	 * This error is not necessary fatal for the current process if it checks
+	 * for successfull malloc operation.
+	 *
+	 * @param task The struct task_t that that was doing the memory allocation.
+	 */
+	void (*oom_error)(struct task_t* task);
+
+  /**
+	 * \brief Stack-Alloc-Error. Called when a context-switch could not allocate
+	 * memory for a process' stack.
+	 *
+	 * This error is fatal for the current process but non-fatal for other
+	 * processes and operating system operation, unless, of cause, if  all
+	 * processes can not be switched out.
+	 *
+	 * @param task The task that failed to be task-switched.
+	 */
+	void (*stack_alloc_fatal)(struct task_t* task);
+	
+	/**
+	 * \brief This hook is called at every timer-interrupt. Which means every 1 ms.
+	 * Watch out what you are doing in this function, since excesive use of
+	 * cpu-ressources will kill the system.
+	 *
+	 * @param time The current value of the micro-second timer.
+	 */
+	void (*timer_event)(uint32_t time);
+
+};
+
+
+// Only provide this macro for the operating system
+#ifdef AOS_KERNEL_MODULE
+
+#define AOS_HOOK(hook,args) if (_aos_hooks && _aos_hooks->hook) { _aos_hooks->hook(args); }
+
+#endif
+
+#endif
+
+
