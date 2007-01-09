f2070172369f0f6b40c66a222dd516af7113a429
Søren Holm <sgh@sgh.dk>
2007-01-09 20:02:01 +0000

    macros

--- src/include/macros.h
+++ src/include/macros.h
@@ -3,17 +3,23 @@
 
 #include <types.h>
 
-#define container_of(VAL,TYPE,MEMBER)  ((TYPE*)((size_t)VAL - offsetof(TYPE,MEMBER)))
 
+// Macros for number-manipulation
 #define ciel(val,max)  (val>max ? max : val)
 #define floor(val,min) (val<min ? min : val)
 #define max(a,b) (a>b ? a : b)
 
+// Macros for initcall-sections
 #define CLASS_MODULE_INIT(function) void* __attribute__((section(".class_initcalls"))) class_initcall_##function = function;
 #define BUS_MODULE_INIT(function) void* __attribute__((section(".bus_initcalls"))) bus_initcall_##function = function;
 #define DRIVER_MODULE_INIT(function) void* __attribute__((section(".driver_initcalls"))) driver_initcall_##function = function;
 #define AOS_MODULE_INIT(function) void* __attribute__((section(".aos_initcalls"))) driver_initcall_##function = function;
 
+// Structure-calculations
+#define container_of(VAL,TYPE,MEMBER)  ((TYPE*)((size_t)VAL - offsetof(TYPE,MEMBER)))
+
+// Compiler stuff
+#define PACKED __attribute__((packed))
 
 /**
  * \brief Calculate the difference between two uint32_t
