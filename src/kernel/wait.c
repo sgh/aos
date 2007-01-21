24a947a810fe30c3ddce86d3377042de1ead6bea
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2007-01-21 23:26:33 +0000

    untested waid_contition implementation
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@170 01777da7-cf65-4787-a2ab-fdced703dd4c

---
+++ src/kernel/wait.c
@@ -0,0 +1,34 @@
+#include <wait.h>
+#include <aos.h>
+
+
+void cond_init(struct condition* cond) {
+	INIT_LIST_HEAD(&cond->waiting);
+	mutex_init(&cond->lock);
+}
+
+void cond_wait(struct condition* cond) {
+	block(&cond->waiting);
+}
+
+void cond_signal(struct condition* cond){
+	mutex_lock(&cond->lock);
+	while (!list_isempty(&cond->waiting)) { // If none is waiting
+		struct task_t* next;
+		next = get_struct_task(list_get_front(&cond->waiting));
+		list_erase(&next->q);
+		unblock(next);
+	}
+	mutex_unlock(&cond->lock);
+}
+
+void cond_broadcast(struct condition* cond) {
+	mutex_lock(&cond->lock);
+	struct task_t* next;
+	while (!list_isempty(&cond->waiting)) { // If none is waiting
+		next = get_struct_task(list_get_front(&cond->waiting));
+		list_erase(&next->q);
+		unblock(next);
+	}
+	mutex_unlock(&cond->lock);
+}
