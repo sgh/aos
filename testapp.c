b54fa2ade7ae5ced6ed392a4de02cfe28a67b5fe
Søren Holm <sgh@sgh.dk>
2007-04-09 23:01:05 +0000

    updated testapp to error-hunting

--- testapp.c
+++ testapp.c
@@ -6,162 +6,124 @@
 #include <atomic.h>
 //#include <serio.h>
 #include <mm.h>
+#include <aos_hooks.h>
 
 struct task_t* task1_cd;
 struct task_t* task2_cd;
+struct task_t* task3_cd;
 struct task_t* idle_cd;
+
+void timer_hook(uint32_t time) {
+	static int count;
+	
+	if (count == 50) {
+		GPIO1_IOPIN ^= BIT24;
+		count = 0;
+	}
+	count++;
+}
+
+struct aos_hooks testapp_aos_hooks = {
+	.timer_event = timer_hook,
+};
 
 mutex_t mymutex;
 
+#define SLEEPUNIT 1
 
-void /*__attribute__((noreturn)) __attribute__((nothrow))*/ task1(void) {
-// 	uint8 c;
-// 	char a[500];
-	int i;
-	int p;
-	char* ptr;
+mutex_t count_lock;
+uint8_t count = 30;
 
-// 	for(;;) {
-// 		GPIO1_IOPIN ^= BIT22;
-// 		p = 0;
-// 		while (p<200000) {
-// 			p++;
-// 		}
-// 	}
-	
+void up(void) {
+// 	mutex_lock(&count_lock);
+// 	count ++;
+// 	mutex_unlock(&count_lock);
+}
+
+void AOS_TASK task1(void) {
 	for (;;) {
-		
-// 		for (i=0; i<sizeof(a); i++)
-// 			a[i] = '1';
-
-// 		for (i=0; i<sizeof(a); i++)
-// 			if (a[i] != '1')
-// 				for (;;);
 		
 		mutex_lock(&mymutex);
-		mutex_unlock(&mymutex);
+		msleep(2);
 		GPIO1_IOSET = BIT22;
-		msleep(75);
+		mutex_unlock(&mymutex);
+		
+		up();
+		msleep(count);
+		
+		
+		mutex_lock(&mymutex);
 		GPIO1_IOCLR = BIT22;
-//  		disable_cs();
-// 		
-
-//  		disable_irqs();
+		mutex_unlock(&mymutex);
 		
-		i = 0;
-		p = 0;
-		while (p<200000) {
-			i++;
-			p++;
-			if (p != i) {
-				disable_cs();
-				for (;;);
-			}
-		}
-// 		enable_irqs();
-// 		enable_cs();
-
-		ptr = malloc(200);
-		msleep(75);
-		free(ptr);
-	}
-}
-
-uint32_t val = 1;
-
-
-void /*__attribute__((noreturn)) __attribute__((nothrow))*/ task2(void) {
-	uint32_t old_val = 0;
-	uint32_t timer = 10000;
-	char a[500];
-	char buf[40];
-	//struct serio console;
-	unsigned int i;
-	char* ptr;
-
-// 	for(;;) ;
-// 	{
-// 		GPIO1_IOPIN ^= BIT23;
-// 		usleep(1000000);
-// 		disable_cs();
-// 		enable_cs();
-// 		disable_irqs();
-// 		enable_irqs();
-// 	}
-
- 	//acquire_serio(0,&console);
-	
-	for(;;) {
+		up();
+		msleep(count);
 		
-		for (i=0; i<sizeof(a); i++)
-			a[i] = '2';
-
-		for (i=0; i<sizeof(a); i++)
-			if (a[i] != '2')
-				for (;;);
-			
-		if (timer == 10000) {
-			old_val = atomic_xchg( &val, old_val);
-
-			if (old_val == 1) mutex_lock(&mymutex);
-			msleep(500);
-			
-// 			snprintf(buf,sizeof(buf),"0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef\r\n");
-// 			serio_put(&console,buf,strlen(buf));
-			
-			if (old_val == 1) mutex_unlock(&mymutex);
-
-
-			if (old_val == 1) {
-				GPIO1_IOSET = BIT23;
-			} else {
-				GPIO1_IOCLR = BIT23;
-			}
-		}
-		ptr = malloc(200);
-		yield();
-		free(ptr);
 	}
 }
 
 
-void /*__attribute__((weak)) __attribute__((noreturn)) __attribute__((nothrow))*/ idle()  {
-// 	uint8_t onoff = 0;
-// 	uint32_t count = 0;
-
-// 	GPIO1_IOPIN |= BIT24;
+void AOS_TASK task2(void) {
 	for (;;) {
-// 		if (count == 0xFFFFF) {
-// 			if (onoff)
-// 				GPIO1_IOSET = BIT24;
-// 			else
-// 				GPIO1_IOCLR = BIT24;
-// 			count = 0;
-// 			onoff ^= 1;
-// 		}
-// 		onoff ^= 1;
-// 		onoff ^= 1;
-//  		//yield();
-// 		count++;
+		
+		mutex_lock(&mymutex);
+		msleep(2);
+		GPIO1_IOSET = BIT23;
+		mutex_unlock(&mymutex);
+		
+		up();
+		msleep(2*count);
+		
+		mutex_lock(&mymutex);
+		GPIO1_IOCLR = BIT23;
+		mutex_unlock(&mymutex);
+		
+		up();
+		msleep(2*count);
+	}
+}
+
+void AOS_TASK task3(void) {
+	for (;;) {
+		mutex_lock(&mymutex);
+		msleep(2);
+		GPIO1_IOSET = BIT24;
+		mutex_unlock(&mymutex);
+
+		up();
+		msleep(4*count);
+		
+		mutex_lock(&mymutex);
+		GPIO1_IOCLR = BIT24;
+		mutex_unlock(&mymutex);
+		
+		up();
+		msleep(4*count);
+	}
+}
+
+
+void AOS_TASK task_arr(uint32_t sleeptime) {
+	for (;;) {
+// 		mutex_lock(&mymutex);
+// 		msleep(1);
+// 		mutex_unlock(&mymutex);
+		msleep(100);
+	}
+}
+
+void AOS_TASK idle()  {
+	for (;;) {
+// 		yield();
 	}
 }
 
 
 
-char __attribute__((aligned(4))) dmem[5*1024];
-
-// struct device lpcuart = {
-// 	.name = "lpcuart",
-// 	.bus = &platform_bus_type,
-// 	.device_id = 1,
-// };
-
-static testinit() {
-// 	for (;;);
-}
+char __attribute__((aligned(4))) dmem[10*1024];
 
 void main(void) {
-
+	int i;
 	//f = sqrtf(f);
 	GPIO1_IODIR |= BIT24|BIT23|BIT22;
 
@@ -173,14 +135,14 @@ void main(void) {
 	
 	task1_cd = create_task(task1, NULL, 0);
 	task2_cd = create_task(task2, NULL, 0);
+// 	task3_cd = create_task(task3, NULL, 0);
 	
+// 	for (i=0; i<2; i++)
+// 		create_task(task_arr, NULL/*(i+2)*2*/, 0);
+	
+	aos_hooks(&testapp_aos_hooks);
 	mutex_init(&mymutex);
-
-	
-// 	for(;;);
+	mutex_init(&count_lock);
 
 	aos_context_init(15000000, idle);
 }
-
-
-AOS_MODULE_INIT(testinit);
