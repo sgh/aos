5135a7cffe3e64ca6396750146477054fc4447a8
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2007-07-12 22:05:36 +0000

    check interrupt
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@256 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -3,6 +3,11 @@
 #include <types.h>
 #include <irq.h>
 #include <interrupt.h>
+#include <assert.h>
+
+#define FIO_BASE_ADDR		0x3FFFC000
+#define FIO2SET        (*(volatile unsigned int *)(FIO_BASE_ADDR + 0x58))
+#define FIO2CLR        (*(volatile unsigned int *)(FIO_BASE_ADDR + 0x5C))
 
 struct irq {
 	void (*isr)(void);
@@ -22,23 +27,42 @@ int irq_attach(int irqnum, void (*isr)(void)) {
 	return 0;
 }
 
+int irq_detach(int irqnum) {
+	interrupt_mask(irqnum);
+	irq_table[irqnum].isr = NULL;
+	return 0;
+}
+
+void irqs_are_disabled(void) {
+// 	FIO2SET = (1<<3);
+}
+
+void irqs_are_enabled(void) {
+// 	FIO2CLR = (1<<3);
+}
+
 void irq_handler(int vector) {
+	ASSERT(irq_table[vector].isr && (vector < 32));
+	
 	irq_table[vector].num_irqs++;
-	//interrupt_enable();
+// 	interrupt_enable();
 	irq_table[vector].isr();
-	//interrupt_disable();
+// 	interrupt_disable();
 }
 
 void irq_lock(void) {
 	uint32_t stat;
 	interrupt_save(&stat);
 	interrupt_disable();
-	if (++nr_irq_lock == 1)
+	irqs_are_disabled();
+	if ((++nr_irq_lock) == 1)
 		saved_irq_state = stat;
 }
 
 void irq_unlock(void) {
-	if (--nr_irq_lock == 0)
+	if ((--nr_irq_lock) == 0) {
 		interrupt_restore(saved_irq_state);
+		irqs_are_enabled();
+	}
 }
 
