6e41916518c22a19f2f57e6eafe01b762f798593
Søren Holm <sgh@sgh.dk>
2007-08-17 07:01:32 +0000

    accept NODEBUG to disable assertions

--- src/include/assert.h
+++ src/include/assert.h
@@ -4,9 +4,17 @@
 #include <aos_hooks.h>
 #include <macros.h>
 
-#define assert(exp) (exp) ? (void)0 : assert_failed(__STRING(exp), __FILE__, __LINE__);
+#ifndef NODEBUG
 
-#define sys_assert(exp) (exp) ? (void)0 : sys_assert_failed(__STRING(exp), __FILE__, __LINE__);
+	#define assert(exp) (exp) ? (void)0 : assert_failed(__STRING(exp), __FILE__, __LINE__);
+	#define sys_assert(exp) (exp) ? (void)0 : sys_assert_failed(__STRING(exp), __FILE__, __LINE__);
+
+#else
+
+	#define assert(exp) 
+	#define sys_assert(exp)
+
+#endif
 
 
 void assert_failed(const char * const exp, const char* const file, unsigned int line);
