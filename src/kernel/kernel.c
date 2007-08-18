c15645e006dde91a4ba97942e90703a2e5be92f8
Søren Holm <sgh@sgh.dk>
2007-08-18 23:26:45 +0000

    Fixed that ugly semaphore-bug. It turned out that sys_block put the current process in the list specified. sys_unblock does the oposite and places the specified task_t in the readyQ. It just forgets to remove the task_t from the list sys_block placed it in. This was not a problem for mutexes where the task_t was removed from the waiting-list in mutex_unlock. sys_unblock now simply removes the task_t from its current waiting-list.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -186,14 +186,14 @@ struct list_head* sys_get_process_list( void )
 void sys_block(struct list_head* q) {
 	list_push_back(q,&current->q);
 	current->state = BLOCKED;
-	sys_assert(&current->q == list_get_back(q));
 	do_context_switch = 1;
-	/** @todo maybe the decition of weither to do context-switch should be done somewhere central */
+	/** @todo maybe the decission of weither to do context-switch should be done somewhere central */
 }
 
 
-void sys_unblock(struct task_t* task) {	
+void sys_unblock(struct task_t* task) {
 	sys_assert(task->state == BLOCKED);
+	list_erase(&task->q);
 	process_wakeup(task);
 }
 
