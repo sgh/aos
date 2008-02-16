f20cc0ce36d9e736531de41b897e308c1e50a71a
Søren Holm <sgh@sgh.dk>
2008-02-16 23:09:35 +0000

    added time for extive key

--- src/kernel/input.c
+++ src/kernel/input.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/select.h>
 
 #define bin(a,b,c,d,e,f,g,h) ((a)*128+(b)*64+(c)*32+(d)*16+(e)*8+(f)*4+(g)*2+(h)*1)
+
+static uint32_t mtime_bias;
+
+void get_sysmtime(uint32_t* time) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	*time = (tv.tv_sec*1000000 + tv.tv_usec)/1000 - mtime_bias;
+}
 
 uint8_t keyscans[] = {
 	bin(0,0,0,0,0,0,0,0),
@@ -9,10 +20,16 @@ uint8_t keyscans[] = {
 	bin(1,0,0,0,0,0,0,0),
 	bin(1,0,0,0,0,0,0,1),
 	bin(1,0,0,0,0,0,0,1),
+	bin(1,0,0,0,0,0,0,1),
+	bin(1,0,0,0,0,0,0,0),
 	bin(1,0,0,0,0,0,0,0),
 	bin(1,0,0,0,0,0,0,1),
+	bin(1,0,0,0,0,0,0,1),
+	bin(1,0,0,0,0,0,1,1),
 	bin(1,0,0,0,0,0,1,1),
 	bin(1,0,0,0,0,1,1,1),
+	bin(1,0,0,0,0,1,1,1),
+	bin(1,0,0,0,0,0,0,0),
 	bin(1,0,0,0,0,0,0,0),
 	bin(0,0,0,0,0,0,0,0),
 	bin(0,0,0,0,0,0,0,0),
@@ -21,14 +38,16 @@ uint8_t keyscans[] = {
 
 static uint8_t additional_keys[2];
 static uint8_t active_key;
+static uint8_t active_time;
 static uint8_t pressed_idx;
 
-void pressedlist_add(uint32_t scancode) {
+void pressedlist_add(uint32_t time, uint32_t scancode) {
 	pressed_idx++;
 	if (pressed_idx > 1)
 		pressed_idx = 0;
 
 	additional_keys[pressed_idx] = active_key = scancode;
+	active_time = time;
 }
 
 void pressedlist_remove(uint32_t scancode) {
@@ -42,15 +61,15 @@ void pressedlist_remove(uint32_t scancode) {
 		active_key = 0;
 }
 
-void scancode_press(uint32_t scancode) {
+void scancode_press(uint32_t time, uint32_t scancode) {
 
-	printf("press:   %4d ", scancode);
+	printf("press(%6u): %-4d ", time, scancode);
 
-	pressedlist_add(scancode);
+	pressedlist_add(time, scancode);
 }
 
 void scancode_release(uint32_t scancode) {
-	printf("release: %4d ", scancode);
+	printf("release: %-4d ", scancode);
 
 	pressedlist_remove(scancode);
 }
@@ -59,10 +78,11 @@ void scancode_release(uint32_t scancode) {
 void register_keyscan(uint8_t keyscan) {
 	static uint8_t last_keyscan;
 	int i;
-	uint32_t bit;
+	uint32_t now = 0;
 	uint32_t scancode = 1;
 	uint8_t press = keyscan & ~last_keyscan;
 	uint8_t release = last_keyscan & ~keyscan;
+	uint8_t scancode_change = (press != 0) | (release |= 0);
 	last_keyscan = keyscan;
 	
 	printf("keyscan:0x%02X ", keyscan);
@@ -75,18 +95,21 @@ void register_keyscan(uint8_t keyscan) {
 		scancode++;
 	}
 	
+	if (press)
+		get_sysmtime(&now);
 	for (scancode = 1; press ; press >>= 1) {
 		if (press & 1)
-			scancode_press(scancode);
+			scancode_press(now, scancode);
 		scancode++;
 	}
 	
-	printf(" %d[", active_key);
-
-	for (i=0; i<2; i++)
-		printf("%d ", additional_keys[i]);
-
-	printf("]\n");
+	if (scancode_change) {
+		printf(" %d[", active_key);
+		for (i=0; i<2; i++)
+			printf("%d ", additional_keys[i]);
+		printf("]\n");
+	} else
+		printf("\n");
 }
 
 
@@ -103,10 +126,15 @@ void register_keyscan(uint8_t keyscan) {
 
 
 
-int main(int argc, const char* argv[]) {
-	int i;
-	for (i = 0; i<sizeof(keyscans); i++)
+int main(/*int argc, const char* argv[]*/) {
+	unsigned int i;
+	
+	get_sysmtime(&mtime_bias);
+	for (i = 0; i<sizeof(keyscans); i++) {
 		register_keyscan(keyscans[i]);
+		usleep(500000);
+	}
 	
 // 	printf("bx: %d\n", bit_count(64+5));
+	return 0;
 }
