0ad2fe344677cf60f7ae42e5c32206c369f86c72
Søren Holm <sgh@sgh.dk>
2008-11-09 22:36:14 +0000

    utf8 build in

--- src/ugui/utf8.c
+++ src/ugui/utf8.c
@@ -1,21 +1,53 @@
+#include <stdio.h>
+#include <ugui/ugui_font.h> 
 
-int decode_utf8(unsigned char** ptr) {
-	unsigned char = **ptr;
-	int code_length = 1;
+int decode_utf8(unsigned char** pptr) {
+	unsigned char* ptr = *pptr;
+	unsigned char c;
+	int code_length = 0;
 	unsigned int value = 0;
-	while (code_length & 0x80) {
-		code_length <<= 1;
-		len++;
-	}
 
-	while (code_length--) {
-		value <<= 8 - code_length;
+	// Wind over UTF-part
+	while ((*ptr & 0xC0) == 0x80)
+		ptr++;
+
+	c = *ptr;
+	while (c & 0x80) {
+		c <<= 1;
+		code_length++;
 	}
 	
+	if (code_length > 1) {
+		value = *ptr << code_length;
+		value &= 0xFF;
+		value >>= code_length;
+// 		printf("UTF code length: %d\n", code_length);
 
-	return;
+		while (--code_length) {
+			ptr++;
+			if (*ptr == 0)
+				return 0;
+			value <<= 6;
+			value |= *ptr & 0x3F;
+		}
+	} else {
+		value = *ptr;
+// 		printf("Ascii: \n");
+	}
+
+// 	printf("0x%02X\n", value);
+	*pptr = ptr+1;
+	return value;
 }
 
 
-int main() {
-}
+// int main() {
+// 	unsigned char buf[6] = {0xC2, 0xA2, 0xE2, 0x82, 0xAC, 0x0};
+// 	unsigned char* ptr = buf;
+// 	int c;
+// 	do {
+// 		c = decode_utf8(&ptr);
+// 		printf("0x%02X\n", c);
+// 	} while (c);
+// 	return 0;
+// }
