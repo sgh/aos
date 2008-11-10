fd8bc473c3bf47374b3a3edff5d30cdf127fe2eb
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2008-11-10 22:11:49 +0000

    improved utf8 decoder
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@422 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/ugui/utf8.c
+++ src/ugui/utf8.c
@@ -1,44 +1,58 @@
 #include <stdio.h>
-#include <ugui/ugui_font.h> 
+#include <ugui/ugui_font.h>
+
+#define UTF8_DEBUG
 
 int decode_utf8(const unsigned char** pptr) {
 	unsigned char* ptr = *pptr;
 	unsigned char c;
 	int code_length = 0;
 	unsigned int value = 0;
-
-	printf("%s:",__FUNCTION__);
+#ifdef UTF8_DEBUG
+	printf("%s:\n",__FUNCTION__);
+	printf("::0x%02X\n", *ptr);
+#endif
 	// Wind over UTF-part
-	while ((*ptr & 0xC0) == 0x80)
+	while ((*ptr & 0xC0) == 0x80) {
 		ptr++;
+#ifdef UTF8_DEBUG
+		printf("::0x%02X\n", *ptr);
+#endif
+	}
 
-	c = *ptr;
-	while (c & 0x80) {
-		c <<= 1;
+	// Count code length
+	for (c = *ptr; c & 0x80; c<<=1)
 		code_length++;
+
+	// Ascii
+	if (code_length==0) { 
+		if (*ptr == 0)
+			*pptr = ptr;
+		else
+			*pptr = ptr + 1;
+		return *ptr;
 	}
 
-	if (code_length > 1) {
-		value = *ptr << code_length;
-		value &= 0xFF;
-		value >>= code_length;
-		printf("UTF code length: %d\n", code_length);
+	value = *ptr << code_length;
+	value &= 0xFF;
+	value >>= code_length;
 
-		while (--code_length) {
-			ptr++;
-			if (*ptr == 0) {
-				*pptr = ptr;
-				return *ptr;
-			}
-			value <<= 6;
-			value |= *ptr & 0x3F;
+	while (--code_length) {
+		ptr++;
+#ifdef UTF8_DEBUG
+		printf("::0x%02X\n", *ptr);
+#endif
+		if (*ptr == 0) {
+			*pptr = ptr;
+			return *ptr;
 		}
-	} else {
-		value = *ptr;
-// 		printf("Ascii: \n");
+		value <<= 6;
+		value |= *ptr & 0x3F;
 	}
 
-	printf("0x%02X\n", value);
+#ifdef UTF8_DEBUG
+	printf("Decoded unicode 0x%02X\n", value);
+#endif
 	*pptr = ptr+1;
 	return value;
 }
