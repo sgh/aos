3a55e2a957c75b8b10375836757a135d1a23d13f
sgh <sgh@01777da7-cf65-4787-a2ab-fdced703dd4c>
2009-03-10 12:51:21 +0000

    added new[] and delete[]
    git-svn-id: svn+ssh://sgh.dk/hdc/svn/aos/trunk@503 01777da7-cf65-4787-a2ab-fdced703dd4c

--- src/memory/newdelete.cpp
+++ src/memory/newdelete.cpp
@@ -9,6 +9,14 @@ void operator delete(void* p) throw() {
   free(p);
 }
 
+void *operator new[] (size_t size) {      // for arays
+  return malloc(size);
+}
+
+void operator delete[] (void* p) {
+  free(p);
+}
+
 void abort(void) {
   // this function is called when an exception occurs.
   // For production code you need to log the message and go to fail-safe
