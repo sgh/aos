d0523078feb88e192d8320076042af878215fac4
Søren Holm <sh@mikrofyn.com>
2010-05-10 15:22:16 +0200

    glyphindex: Remember all glyphs.

--- src/ugui/glyphindex.c
+++ src/ugui/glyphindex.c
@@ -6,56 +6,43 @@
 unsigned char active_characters[65536/8];
 
 int main() {
-  unsigned char buf[16];
+  unsigned char buf[1024];
   int idx = 0;
-//   struct unicode_parser utf8;
-//   uint32_t uc;
-//   unsigned char* buf = "ﺍﻺﻋﺩﺍﺩ XC-2";
-// 
-//   unicode_init(&utf8, buf);
-// 
-//   do {
-//     uc = unicode_current(&utf8);
-//     printf("%5X",uc);
-//   } while (unicode_next(&utf8));
-//   printf("\n");
-// 
-//   do {
-//     uc = unicode_current(&utf8);
-//     printf("%5X",uc);
-//   } while (unicode_prev(&utf8));
-//   printf("\n");
-// 
-//   return 0;
+
 	memset(active_characters, 0, sizeof(active_characters));
 	while (!feof(stdin)) {
 		buf[idx] = getchar();
 
-		// Unicode characters must be collected in a signle chunk
-		if (buf[idx] > 0xC0)
+		// Unicode characters must be collected in a single chunk
+		if (buf[idx] & 0x80)
 			idx++;
 		else { // Normal ascii char or end of utf8
 			// End of utf8
 			if (idx > 0) {
 				struct unicode_parser utf8;
-				unicode_init(&utf8, buf);
-				unsigned uc = unicode_current(&utf8);
-				active_characters[uc>>3] |= 1<<(uc&0x7);
-// 				printf("%d ", uc);
+				buf[idx+1] = 0;
+				unicode_init(&utf8, (const char*)buf);
+				do {
+					unsigned uc = unicode_current(&utf8);
+					active_characters[uc>>3] |= 1<<(uc&0x7);
+// 					printf("%d ", uc);
+				} while (unicode_next(&utf8));
 				idx = 0;
-			} else // Normal character
+			} else { // Normal character
 				active_characters[buf[idx]>>3] |= 1<<(buf[idx]&0x7);
+// 				printf("%c : %d\n", buf[idx], buf[idx]);
+			}
 		}
 	}
 
 	int i;
 	int b;
 	for (i=0; i<65536/8; i++) {
-		for (b=0; b<7; b++) {
+		for (b=0; b<8; b++) {
 			if (active_characters[i] & 1<<b)
 				printf("%d\n", i*8+b);
 		}
 	}
-	
+	return 0;
 }
 
