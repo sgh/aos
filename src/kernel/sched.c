9c46d339e9ed2ab3445b09b9010e338bf9de5793
Søren Holm <sh@mikrofyn.com>
2012-01-15 11:50:37 +0100

    Enable interruots during stack copy.
    This solves too late servicing of interrupts
    resulting in loss of uart bytes etc.

--- src/kernel/sched.c
+++ src/kernel/sched.c
@@ -132,6 +132,9 @@ static FLATTEN HOT void sched_switch(void) {
 	// tell mm-system not to use scheck_lock
 	mm_schedlock(0);
 
+	// Enable interrupts
+	interrupt_enable();
+
 	prev->stack_size = (uint32_t)&__stack_usr_top__ - prev->ctx.uregs->sp;
 
 	if (prev->stack_size > prev->max_stack_size) {
@@ -149,6 +152,9 @@ static FLATTEN HOT void sched_switch(void) {
 
 	if (next->fragment)
 		load_fragment(&__stack_usr_top__ - next->stack_size, next->fragment);
+
+        // Disable interrupts again
+        interrupt_disable();
 
 	// Tell mm-system to use schedlock
 	mm_schedlock(1);
